8eccd69ec27721672dacc2f33f4c39605bf3b160
zeng715734199 <715734199@qq.com>
2026-07-03 16:29:52 +0800

    feat: 尝试写一个user/zlh的get 接口

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,17 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('user')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('zlh')
+  getZlh(): any {
+    return this.appService.getZlh();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,9 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello zlh!';
+  }
+  getZlh(): any {
+    return { code: 200, msg: '我叫曾林煌' };
   }
 }
